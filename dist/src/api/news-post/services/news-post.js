aa25bfdb95b0d97b744e3953992a6328dbbe29cd
Mukhtarkhan Baibesh <baibesh91@gmail.com>
2022-10-04 11:18:10 +0600

    added some endpoints

---
+++ dist/src/api/news-post/services/news-post.js
@@ -0,0 +1,7 @@
+"use strict";
+/**
+ * news-post service
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const strapi_1 = require("@strapi/strapi");
+exports.default = strapi_1.factories.createCoreService('api::news-post.news-post');
