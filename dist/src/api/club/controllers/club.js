bf0f6556e8c55b8b0a2683498d6be56eaf2154ad
Mukhtarkhan Baibesh <baibesh91@gmail.com>
2022-11-15 08:25:24 +0600

    added club and tutor

---
+++ dist/src/api/club/controllers/club.js
@@ -0,0 +1,7 @@
+"use strict";
+/**
+ * club controller
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const strapi_1 = require("@strapi/strapi");
+exports.default = strapi_1.factories.createCoreController('api::club.club');
